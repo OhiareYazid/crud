478c9970fe6cdf578b99fdc4908d4c111d4be0f9
OhiareYazid <isahyazid@yahoo.com>
2023-09-15 12:00:44 +0100

    hello finished oo

--- src/controllers/personController.js
+++ src/controllers/personController.js
@@ -1,35 +1,46 @@
 // controllers/personController.js
-const Person = require('../models/personModel');
+const Person = require("../models/personModel");
 
 // Create a new person
-exports.createPerson = (req, res) => {
+exports.createPerson = async (req, res) => {
+  console.log(req.body);
   const newPerson = new Person(req.body);
-  newPerson.save((err, person) => {
-    if (err) res.status(400).send(err);
-    res.json(person);
-  });
+  try {
+    const person = await newPerson.save();
+    return res.json(person);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 // Get details of a person by user_id
-exports.getPerson = (req, res) => {
-  Person.findById(req.params.user_id, (err, person) => {
-    if (err) res.status(404).send('Person not found');
-    res.json(person);
-  });
+exports.getPerson = async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.user_id);
+
+    return res.json(person);
+  } catch (error) {
+    res.status(404).send("Person not found");
+  }
 };
 
 // Update details of an existing person by user_id
 exports.updatePerson = (req, res) => {
-  Person.findByIdAndUpdate(req.params.user_id, req.body, { new: true }, (err, person) => {
-    if (err) res.status(400).send(err);
-    res.json(person);
-  });
+  Person.findByIdAndUpdate(
+    req.params.user_id,
+    req.body,
+    { new: true },
+    (err, person) => {
+      if (err) res.status(400).send(err);
+      res.json(person);
+    }
+  );
 };
 
 // Remove a person by user_id
 exports.deletePerson = (req, res) => {
   Person.findByIdAndRemove(req.params.user_id, (err) => {
-    if (err) res.status(404).send('Person not found');
-    res.send('Person deleted successfully');
+    if (err) res.status(404).send("Person not found");
+    res.send("Person deleted successfully");
   });
 };
