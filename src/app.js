4d1b7898eea1d85c06114d716aea06063eb12a36
Yazid Ohiare <35582610+OhiareYazid@users.noreply.github.com>
2023-09-15 09:23:10 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 // app.js
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const uri = "mongodb+srv://isahyazid1:EnHPBJEgZ7TsaWv@cluster0.g7m2pzi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
 
-mongoose.connect('mongodb+srv://isahyazid1:iEnHPBJEgZ7TsaWv@cluster0.g7m2pzi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error: ' + err);
-  process.exit(-1);
-});
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
