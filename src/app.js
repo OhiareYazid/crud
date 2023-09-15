5bcb389d6921d6ac5e123e31673301b065d22c27
Yazid Ohiare <35582610+OhiareYazid@users.noreply.github.com>
2023-09-15 09:09:23 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
-mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://isahyazid1:iEnHPBJEgZ7TsaWv@cluster0.g7m2pzi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', (err) => {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
@@ -12,6 +12,11 @@ mongoose.connection.on('error', (err) => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// app.js
+const personRoutes = require('./routes/personRoutes');
+app.use('/api', personRoutes);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
